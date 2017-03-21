d188dc9f10acd2a32269e40a3f47eaf2b0ae6c27
Bogdan <badbog76@yahoo.com>
2017-03-21 21:47:05 +0200

    celsius

---
+++ src/celsius.java
@@ -0,0 +1,12 @@
+/**
+ * Created by xyz on 21-Mar-17.
+ */
+public class celsius {
+    public static void main(String[] args) {
+        double x,y;
+        x=SkeletonJava.readDoubleConsole(" Celsius = ");
+        y=(x*9/5)+32;
+        System.out.println( x+" Celsius = "+y+" degrees Fahrenheit ");
+
+    }
+}
