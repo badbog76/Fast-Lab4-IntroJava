bf2249214c54cc207a02efc95d2c65add493e63c
Bogdan <badbog76@yahoo.com>
2017-03-21 21:32:30 +0200

    cat de cat

---
+++ src/numere0suma.java
@@ -0,0 +1,17 @@
+/**
+ * Created by xyz on 21-Mar-17.
+ */
+public class numere0suma {
+    public static void main(String[] args) {
+        int x,y; y=0;
+        do {
+            x=SkeletonJava.readIntConsole(" X =");
+            y=y+x;
+
+        }
+        while ( x!=0);
+
+
+        System.out.println(" suma= " +y);
+    }
+}
